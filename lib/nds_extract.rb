5006abc123119303e122f7a6954aa000c9abd3dc
Joseph Avila <josepharthuravila@gmail.com>
2020-01-05 00:47:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,7 +26,7 @@ def list_of_directors(source)
   array = []
   index = 0 
   while index < source.length do
-    array << source.fetch[index]
+    array << source.fetch(:name)[index]
     index += 1 
   end
   p array
