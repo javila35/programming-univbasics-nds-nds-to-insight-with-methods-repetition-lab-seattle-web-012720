8f6f51ee7f249a3d9e13ad0de7db63bd4bdca0a7
Joseph Avila <josepharthuravila@gmail.com>
2020-01-05 00:52:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -45,7 +45,7 @@ def total_gross(source)
 total = 0 
 index = 0
 while index < list_of_directors(source).length do 
-  total += directors_totals(source)[index]
+  p directors_totals(source)
   index += 1
 end 
 total 
